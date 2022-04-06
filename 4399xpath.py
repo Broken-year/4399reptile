abad330b7e5e0834e76325db4f240774bc9b913a
Administrato <2011416908@qq.com>
2022-04-06 08:51:42 +0800

    xpath的使用,以及突破反爬机制的理论

--- 4399xpath.py
+++ 4399xpath.py
@@ -1,6 +1,6 @@
 import requests
 from lxml import etree
-
+import random
 
 #页面内爬虫
 def pachong(url):
@@ -50,15 +50,21 @@ def find_next_page(url):
 
 def main():
     url="http://www.4399dmw.com/search/dh-1-0-0-0-0-0-0/"
-    while True:
-        try:
-            print("开始爬行的url:"+url)
-            pachong(url)
-            url = find_next_page(url)
-            print('----------------------------')
-        except:
-            break
-    print("最后一页！")
+    # while True:
+    #     try:
+    #         print("开始爬行的url:"+url)
+    #         pachong(url)
+    #         url = find_next_page(url)
+    #         print('----------------------------')
+    #     except:
+    #         break
+    # print("最后一页！")
+
+    version_id = random.randint(50,100)
+    os_type = ['Windows NT 10.0','Windows NT6.1','linux 10.0']
+
+
+
 
     """
     xpath语法
@@ -87,3 +93,29 @@ def main():
 if __name__ == '__main__':
     main()
 
+"""
+反爬虫
+
+通过user-agent
+    ua="Mozilla/5.0 ("+random.choice(os_type)+"; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987."+str(version_id)+" Safari/537.36",
+    #ua=''.join(['Mozilla/5.0',random.choice(os_type),"bAppleWebKit/537.36 (KHTML, like Gecko)","Chrome/57.0.2987.",str(version_id)," Safari/537.36"])
+
+通过Referer
+
+通过cookie
+考虑建立cookie池(多账户)
+
+通过ip
+使用代理ip等方法
+
+验证码
+考虑写程序识别验证码,打码平台
+
+通过自定义字体
+1.有的网站通过审查元素字体无法显示,显示为口,考虑使用手机网络访问
+2.通过字体偏移
+ABCDEFG....
+CDEFGHI....
+
+
+"""
